a7e14d12063db0131a0521603e1703f1e665f13e
Cesar-po1 <Cesar-po1@users.noreply.github.com>
2020-06-03 05:28:54 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -117,7 +117,7 @@ void record_preferences(int ranks[])
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            if (ranks[i] > ranks[j])
+            if (ranks[i] < ranks[j])
             {
                 preferences[i][j] += 1;
             }
