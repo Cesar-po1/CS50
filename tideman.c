7dc63b81b85287bd5e3577ab11a17ff68b1db644
Cesar-po1 <Cesar-po1@users.noreply.github.com>
2020-06-04 14:54:56 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -135,8 +135,8 @@ void add_pairs(void)
         {
             if (preferences[i][j] > preferences[j][i])
             {
-                pairs[pair_count].winner += preferences[i][j];
-                pairs[pair_count].loser += preferences[j][i];
+                pairs[pair_count].winner = pairs[pair_count].winner + preferences[i][j];
+                pairs[pair_count].loser = pairs[pair_count].loser + preferences[j][i];
                 pair_count++;
             }
         }
