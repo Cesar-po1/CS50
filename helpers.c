8eb2c09ffd1b0e829e3923303f9d18f7848eb43e
Cesar-po1 <Cesar-po1@users.noreply.github.com>
2020-06-17 15:52:15 +0000

    automated commit by check50 [check50=True]

---
+++ helpers.c
@@ -0,0 +1,98 @@
+#include "helpers.h"
+#include <stdio.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int promBnW = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue)/3;
+            
+            image[i][j].rgbtRed = promBnW;
+            image[i][j].rgbtGreen = promBnW;
+            image[i][j].rgbtBlue = promBnW;
+        }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int tempRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
+            int tempGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
+            int tempBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
+            
+            if (tempRed > 255)
+            {
+                tempRed = 255;
+            }
+            else if (tempRed < 0)
+            {
+                tempRed = 0;
+            }
+            
+            if (tempGreen > 255)
+            {
+                tempGreen = 255;
+            }
+            else if (tempGreen < 0)
+            {
+                tempGreen = 0;
+            }
+            
+            if (tempBlue > 255)
+            {
+                tempBlue = 255;
+            }
+            else if (tempBlue < 0)
+            {
+                tempBlue = 0;
+            }
+            image[i][j].rgbtRed = tempRed;
+            image[i][j].rgbtGreen = tempGreen;
+            image[i][j].rgbtBlue = tempBlue;
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width/2 ; j++)
+        {
+            int tempRed = image[i][j].rgbtRed;
+            int tempGreen = image[i][j].rgbtGreen;
+            int tempBlue = image[i][j].rgbtBlue;
+            
+            image[i][j].rgbtRed = image[i][width - 1 - j].rgbtRed;
+            image[i][j].rgbtGreen = image[i][width - 1 - j].rgbtGreen;
+            image[i][j].rgbtBlue = image[i][width - 1 - j].rgbtBlue;
+            
+            image[i][width - 1 - j].rgbtRed = tempRed;
+            image[i][width - 1 - j].rgbtGreen = tempGreen;
+            image[i][width - 1 - j].rgbtBlue = tempBlue;
+        }
+    }
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width ; j++)
+        {
+            int promBlurR = image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue)/3;
+    return;
+}
