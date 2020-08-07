ab32a82027e67ddcd7238c1e3c9b9225825736ca
Cesar-po1 <Cesar-po1@users.noreply.github.com>
2020-08-07 05:37:02 +0000

    automated commit by submit50

--- hello.py
+++ hello.py
@@ -1,2 +1,3 @@
-name = input("What is your name?\n") #asks for name
+from cs50 import get_string
+name = get_string("What is your name?\n") #asks for name
 print("hello,",name) #puts together hello and name
