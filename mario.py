2871cc5af98abff3c212e9bc55684903f9cc23a3
Cesar-po1 <Cesar-po1@users.noreply.github.com>
2020-08-07 05:54:56 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,22 +1,23 @@
-from cs50 import get_int
+from cs50 import get_int  # imports library
 
 i, j, k, height = 0, 0, 0, 0 #declare variables
 
 while True:
-    height = get_int("Height: ") #asks for height
+    height = get_int("Height: ")  # asks for height
     if height < 9 and height > 0:
         break
 
-for i in range (0, height, 1): #itinerates rows
-    for k in range (height-1, -1, -1):  #1st piramid colums
+for i in range (0, height, 1):  # itinerates rows
+    for k in range (height - 1, -1, -1):  # 1st piramid colums
         if i < k:
             print(" ", end="")
         else:
             print("#", end="")
-    print("  ", end="") #second piramid colums
+    print("  ", end="")  # second piramid colums
     for j in range (-1, height - 1, 1):
         if i > j:
             print("#", end="")
         else:
             print(" ", end="")
-    print() #change 
+            break
+    print()  # change 
