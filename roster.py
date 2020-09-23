162a3d67983e4120e4ed29a5670cb642b661365e
Cesar-po1 <Cesar-po1@users.noreply.github.com>
2020-09-23 06:40:16 +0000

    automated commit by submit50

---
+++ roster.py
@@ -0,0 +1,12 @@
+# TODO
+import cs50
+from sys import argv
+
+if len(argv) != 2:  
+    print('Usage: 2 command-line agruments')  #checks user input
+    exit(1)
+    
+db = cs50.SQL("sqlite:///students.db") #sets de data base
+rows = db.execute("SELECT * FROM students WHERE house = ? ORDER BY last, first", argv[-1]) #query
+for name in rows:
+    print(name['first'] + ' ' + (name['middle'] + ' 'if name['middle'] != None else'') + name['last'] + ',' , 'born' , name['birth']) #outpu
