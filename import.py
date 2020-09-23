b63118114d04b18ee522e9daa28ba94ddfd34e6a
Cesar-po1 <Cesar-po1@users.noreply.github.com>
2020-09-23 06:42:54 +0000

    automated commit by submit50

--- import.py
+++ import.py
@@ -7,11 +7,11 @@ if len(argv) != 2:
     print('Usage: 2 command-line agruments')
     exit(1)
 
-db = cs50.SQL("sqlite:///students.db") #database
-with open(argv[1],'r') as file:
-    reader = csv.DictReader(file) #readsfile
+db = cs50.SQL("sqlite:///students.db")  # database
+with open(argv[1], 'r') as file:
+    reader = csv.DictReader(file)  # readsfile
     for row in reader:
-        names = row['name'].split() #for middle name
+        names = row['name'].split()  # for middle name
         if len(names) == 3:
             first = names[0]
             middle = names[1]
@@ -22,5 +22,6 @@ with open(argv[1],'r') as file:
             last = names[1]
         house = row['house']
         birth = row['birth']
-        db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES (?,?,?,?,?)",first, middle, last, house, birth) #query
+        db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES (?,?,?,?,?)",
+                    first, middle, last, house, birth)  # query
         
